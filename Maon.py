7cefdf820b2deb3ffc089895525c32beaa59cdc8
RaeNon <44230124+RaeNon@users.noreply.github.com>
2020-03-08 01:25:24 +0100

    Rewrite

---
+++ Maon.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import discord
+import configuration as config
+import login as login
+
+
+class Maon:
+    __slots__ = "client"
+
+    def __init__(self):
+        print(config.SIGNATURE)
+        print("Discord.py Version: {}".format(discord.__version__))
+        self.client = commands.Bot(command_prefix=config.PREFIX, case_insensitive=True, owner_id=config.OWNER_ID)
+        self.client.remove_command("help")
+
+    def load_extensions(self):
+        for ext in config.EXTENSION_LIST:
+            print("Loading {} extension...".format(ext))
+            self.client.load_extension(config.EXTENSION_PATH + ext)
+
+    def run(self):
+        self.client.run(login.TOKEN)
+
+
+Maon = Maon()
+Maon.load_extensions()
+Maon.run()
