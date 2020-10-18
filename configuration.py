bcc5c0bd954b25dc8c3ba51e2e5128a3e7b4919d
raesoft <44230124+raesoft@users.noreply.github.com>
2020-10-18 22:41:51 +0200

    feat/queue Added playlist command, reworked help embed, added Usage to browser already running message

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 # Prefix And Embed Color:
 PREFIX = [
-    "m ", "M ", "maon ", "Maon ", "mAon ", "maOn ", "maoN ", "MAon ", "mAOn ", "maON ", "MAOn ", "mAON ", "MAON "
+    "maon ", "m ", "M ", "Maon ", "mAon ", "maOn ", "maoN ", "MAon ", "mAOn ", "maON ", "MAOn ", "mAON ", "MAON "
 ]
 PREFIX_FAST = "maon"
 COLOR_HEX = 0xf8d386
@@ -17,6 +17,8 @@ TEMP_FOLDER_MAX_SIZE_IN_MB = 512
 SFX_VOLUME = 0.3            # Volume of special effects
 PLAYER_TIMEOUT = 7200       # Seconds until Maon disconnects from a voice channel without any interaction
 SONG_DURATION_MAX = 600     # How long songs can be in seconds to be downloaded and stored locally
+
+DOWNLOAD_RATE_LIMITER = "3M"    # Limit the bandwith when Maon downloads songs (e.g. 3M for 3 MegabBytes / s)
 
 # Activity Texts:
 STATUS_TEXT_LISTENING_TO = [
@@ -79,7 +81,10 @@ AUDIO_DOWNLOAD_CMD_DEFAULT = [
     TEMP_PATH[2:] + "%(title)s-%(id)s.%(ext)s",
     "-f",
     "format id goes into 10",
-    "url goes into 11"
+    "url goes into 11", 
+    "--limit-rate",
+    DOWNLOAD_RATE_LIMITER
+
 ]
 
 BEFORE_ARGS = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 10"
@@ -117,44 +122,45 @@ COMMANDLIST_EMBED_PREP_START = "Prefix: Maon (case insensitive)\n\n"
 
 COMMANDLIST_EMBED_ADMIN_PREP = [
     ":flag_kp: ***Admin Commands:*** (requires ownership)\n",
-    ":white_small_square: kill - Shuts Maon down.\n",
-    ":white_small_square: reload <extension / all> - Unloads and loads extensions.\n",
-    ":white_small_square: remove <number> - Removes messages in the channel.\n",
-    ":white_small_square: status <listening/playing/watching> <status> - Sets Maon's status.\n",
-    ":white_small_square: status cancel - Cancels Maon's looping status updates.\n",
-    ":white_small_square: download <audio/video> <url> - Downloads a full Youtube video or just the audio.\n",
+    ":white_small_square: " + PREFIX[0] + "kill - Shuts Maon down.\n",
+    ":white_small_square: " + PREFIX[0] + "reload <extension / all> - Unloads and loads extensions.\n",
+    ":white_small_square: " + PREFIX[0] + "remove <number> - Removes messages in the channel.\n",
+    ":white_small_square: " + PREFIX[0] + "status <listening/playing/watching> <status> - Sets Maon's status.\n",
+    ":white_small_square: " + PREFIX[0] + "status cancel - Cancels Maon's looping status updates.\n",
     "\n"
 ]
 
 COMMANDLIST_EMBED_PREP = [
     ":beginner: ***Useful Commands:***\n",
-    ":white_small_square: ping - Maon's latency.\n",
-    ":white_small_square: help / info - What you're looking at right now.\n",
+    ":white_small_square: " + PREFIX[0] + "ping - Maon's latency.\n",
+    ":white_small_square: " + PREFIX[0] + "help - What you're looking at right now.\n",
     "\n",
     ":notes: ***Music Commands:***\n",
-    ":white_small_square: browse <music / sfx> - Opens the file browser.\n",
-    ":white_small_square: play <link / filepath> - Plays a Youtube video or local file.\n",
-    ":white_small_square: sfx <link / filepath> - Plays a local sound effect.\n",
-    ":white_small_square: stop / leave - Turns off the music player.\n",
-    ":white_small_square: skip - Skips the current song.\n",
-    ":white_small_square: loop <song / queue> - Loops the current song or queue.\n",
-    ":white_small_square: volume / vol <number 0 - 100> - Changes the music player volume.\n",
-    ":white_small_square: pause - Pauses the music player.\n",
-    ":white_small_square: resume / cont / continue - Resumes the music player.\n",
+    ":white_small_square: " + PREFIX[0] + "browse <music / sfx> - Opens the file browser.\n",
+    ":white_small_square: " + PREFIX[0] + "play <link / filepath / filename> - Plays a Youtube video or local file.\n",
+    ":white_small_square: " + PREFIX[0] + "sfx <filepath / filename> - Plays a local sound effect.\n",
+    ":white_small_square: " + PREFIX[0] + "playlist - Displays the current playlist.\n",
+    ":white_small_square: " + PREFIX[0] + "playlist <1 - 15> - Puts an entry to the front.\n",
+    ":white_small_square: " + PREFIX[0] + "stop - Turns off the music player and makes Maon leave.\n",
+    ":white_small_square: " + PREFIX[0] + "skip - Skips the current song.\n",
+    ":white_small_square: " + PREFIX[0] + "loop <song / playlist> - Loops the current song or playlist.\n",
+    ":white_small_square: " + PREFIX[0] + "volume <0 - 100> - Changes the music player volume.\n",
+    ":white_small_square: " + PREFIX[0] + "pause - Pauses the music player.\n",
+    ":white_small_square: " + PREFIX[0] + "resume - Resumes the music player.\n",
     "\n",
     ":juggling: ***Fun Commands:***\n",
-    ":white_small_square: <question> - Maon will reply to your question.\n",
-    ":white_small_square: toss - Coin toss.\n",
-    ":white_small_square: roll / rng / dice <number> - Roll a number.\n"
+    ":white_small_square: " + PREFIX[0] + "<question> - Maon will reply to a closed (is, are, do, can...) question.\n",
+    ":white_small_square: " + PREFIX[0] + "toss - Coin toss.\n",
+    ":white_small_square: " + PREFIX[0] + "roll <number> - Roll a number from 0 to number.\n"
 ]
 
 # Maon Version:
 SIGNATURE = '''
-   __                __            
-  /__ \__ _  ___  /\ \ \___  _ __  
- / \/ / _` |/ _ \/  \/ / _ \| '_ \ 
-/ _  \ (_| |  __/ /\  / (_) | | | |
-\/ \_/\__,_|\___\_\ \/ \___/|_| |_|
-          Maon v20.09.14
+                            __ _   
+ _ __ __ _  ___  ___  ___  / _| |_ 
+| '__/ _` |/ _ \/ __|/ _ \| |_| __|
+| | | (_| |  __/\__ \ (_) |  _| |_ 
+|_|  \__,_|\___||___/\___/|_|  \__|
+          Maon v20.10.18
 '''
-VERSION = "Maon v20.09.14"
+VERSION = "Maon v20.10.18"
